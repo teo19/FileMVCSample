654098fdc073d83b8ef951cca04fd57a9d8a79a0
teo19 <teo.uribe.m@gmail.com>
2016-09-13 10:36:11 -0500

    initi commit

---
+++ FileManagerMVC/Controllers/ArchivosController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FileManagerMVC.Models;
+using System.Data.Entity.Infrastructure;
+
+namespace FileManagerMVC.Controllers
+{
+    public class ArchivosController : Controller
+    {
+        private FilesMVCEntities db = new FilesMVCEntities();
+
+        // GET: Archivos
+        public ActionResult Index()
+        {
+            var archivo = db.Archivo.Include(a => a.Extension);
+            return View(archivo.ToList());
+        }
+
+        // GET: Archivos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Archivo archivo = db.Archivo.Find(id);
+            if (archivo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(archivo);
+        }
+
+        // GET: Archivos/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id_extencion = new SelectList(db.Extension, "id", "Nombre");
+            return View();
+        }
+
+        // POST: Archivos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Nombre,Id_extencion,Data")]  Archivo archivo, HttpPostedFileBase uploadFile)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (uploadFile != null && uploadFile.ContentLength > 0)
+                    {
+                        using (var reader = new System.IO.BinaryReader(uploadFile.InputStream))
+                        {
+                            archivo.Data = reader.ReadBytes(uploadFile.ContentLength);
+                        }
+                    }
+                    db.Archivo.Add(archivo);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (RetryLimitExceededException e)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            ViewBag.Id_extencion = new SelectList(db.Extension, "id", "Nombre", archivo.Id_extencion);
+            return View(archivo);
+
+            /*
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var avatar = new Archivo
+                        {
+                            FileName = System.IO.Path.GetFileName(upload.FileName),
+                            FileType = FileType.Avatar,
+                            Data = file.ContentType
+                        };
+                        using (var reader = new System.IO.BinaryReader(upload.InputStream))
+                        {
+                            avatar.Content = reader.ReadBytes(upload.ContentLength);
+                        }
+                        student.Files = new List<File> { avatar };
+                    }
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (RetryLimitExceededException  dex )
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(student);*/
+        }
+
+        // GET: Archivos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Archivo archivo = db.Archivo.Find(id);
+            if (archivo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id_extencion = new SelectList(db.Extension, "id", "Nombre", archivo.Id_extencion);
+            return View(archivo);
+        }
+
+        // POST: Archivos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Nombre,Id_extencion,Data")] Archivo archivo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(archivo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id_extencion = new SelectList(db.Extension, "id", "Nombre", archivo.Id_extencion);
+            return View(archivo);
+        }
+
+        // GET: Archivos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Archivo archivo = db.Archivo.Find(id);
+            if (archivo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(archivo);
+        }
+
+        // POST: Archivos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Archivo archivo = db.Archivo.Find(id);
+            db.Archivo.Remove(archivo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FileManagerMVC/Controllers/ExtensionesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FileManagerMVC.Models;
+
+namespace FileManagerMVC.Controllers
+{
+    public class ExtensionesController : Controller
+    {
+        private FilesMVCEntities db = new FilesMVCEntities();
+
+        // GET: Extensiones
+        public ActionResult Index()
+        {
+            return View(db.Extension.ToList());
+        }
+
+        // GET: Extensiones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Extension extension = db.Extension.Find(id);
+            if (extension == null)
+            {
+                return HttpNotFound();
+            }
+            return View(extension);
+        }
+
+        // GET: Extensiones/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Extensiones/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Nombre")] Extension extension)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Extension.Add(extension);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(extension);
+        }
+
+        // GET: Extensiones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Extension extension = db.Extension.Find(id);
+            if (extension == null)
+            {
+                return HttpNotFound();
+            }
+            return View(extension);
+        }
+
+        // POST: Extensiones/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Nombre")] Extension extension)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(extension).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(extension);
+        }
+
+        // GET: Extensiones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Extension extension = db.Extension.Find(id);
+            if (extension == null)
+            {
+                return HttpNotFound();
+            }
+            return View(extension);
+        }
+
+        // POST: Extensiones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Extension extension = db.Extension.Find(id);
+            db.Extension.Remove(extension);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
